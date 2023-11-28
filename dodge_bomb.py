b44000aee90a6a251a4f57f7ac4562d19b00a582
c0b22115 <c0b22115b0@edu.teu.ac.jp>
2023-11-28 14:15:09 +0900

    練習2:爆弾Surfaceを移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,8 +1,9 @@
+import random
 import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1100, 600
 
 
 def main():
@@ -11,6 +12,14 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img = pg.Surface((20, 20))#練習１:透明のSurfaceをつくる
+    bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb_img.get_rect()#練習２　爆弾Surfaceを作る
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -20,9 +29,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
